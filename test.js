e26d6783bf1933dd1af7167d0892fd552b06d237
Alex <iosdevcoff@gmail.com>
2023-03-15 18:31:19 -0400

    submit works

--- test.js
+++ test.js
@@ -1,30 +1,19 @@
-console.log("Hello World!");
-// Create a JS class called Inventory with the following properties:
-// - cpm - cost per mille
-// - audeince - audience
-class Inventory {
-    constructor(cpm, audience) {
-      this.cpm = cpm;
-      this.audience = audience;
+// Class that has three fields: preRollPrice, midRollPrice, postRollPrice
+class SegmentPrice {
+    constructor(preRollPrice, midRollPrice, postRollPrice) {
+        this.preRollPrice = preRollPrice;
+        this.midRollPrice = midRollPrice;
+        this.postRollPrice = postRollPrice;
     }
 }
 
-// Function to calculate the total cost from an array of Inventory objects
-function calculateCost(inventory) {
-    return inventory.reduce((total, item) => {
-      return total + (item.cpm * item.audience);
-    }, 0);
+function intFromText(text) {
+    var number = parseInt(text);
+    return isNaN(number) ? 0 : number;
 }
 
-// Create an array of Inventory objects
-const inventory = [
-    new Inventory(100, 1000),
-    new Inventory(200, 2000),
-    new Inventory(300, 3000),
-    new Inventory(400, 4000),
-];
-  
-// Calculate the total cost
-function test() {
-    alert(calculateCost(inventory));
+function isElementActive(elementId) {
+    const element = document.querySelector(elementId);
+    const containsActive = element.classList.contains("active");
+    return containsActive;
 }
